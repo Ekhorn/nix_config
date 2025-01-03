8f2846728a25d8d3b8b6764a112726fdc9c19808
Ekhorn <koen@kschellingerhout.nl>
2025-01-03 07:00:04 +0100

    conf(zsh): add nixos-rebuild aliases

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -10,6 +10,12 @@
     enableCompletion = true;
     syntaxHighlighting.enable = true;
 
+    shellAliases = {
+      build = "sudo nixos-rebuild test";
+      switch = "sudo nixos-rebuild switch";
+      update = "sudo nixos-rebuild switch --recreate-lock-file";
+    };
+
     oh-my-zsh = {
       enable = true;
       theme = "robbyrussell";
