f867e6a317628de518d667df2cdd11c1c6a85312
Ekhorn <koen@kschellingerhout.nl>
2025-01-10 07:31:17 +0100

    conf: add gh pub key to other servers

--- anywhere/aws/configuration.nix
+++ anywhere/aws/configuration.nix
@@ -11,6 +11,7 @@
   user.enable = true;
   user.username = "spaced";
   user.extraGroups = ["wheel" "docker"];
+  user.extraKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+BFpsmOaKQW92nRePSxW/FN40mPPREQ+UeX9FkuAQU"];
   user.shell = pkgs.bash;
 
   system.stateVersion = "24.11";
--- anywhere/do/configuration.nix
+++ anywhere/do/configuration.nix
@@ -11,6 +11,7 @@
   user.enable = true;
   user.username = "spaced";
   user.extraGroups = ["wheel" "docker"];
+  user.extraKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+BFpsmOaKQW92nRePSxW/FN40mPPREQ+UeX9FkuAQU"];
   user.shell = pkgs.bash;
 
   # do not use DHCP, as DigitalOcean provisions IPs using cloud-init
