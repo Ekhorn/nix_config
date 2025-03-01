cefbdcde115b1ea2d02385d41195eca3b5b8c978
Ekhorn <koen@kschellingerhout.nl>
2025-03-01 23:04:23 +0100

    conf: keepassxc browser integration

--- modules/home-manager/keepassxc.nix
+++ modules/home-manager/keepassxc.nix
@@ -6,9 +6,21 @@
       [General]
       ConfigVersion=2
 
+      [Browser]
+      CustomProxyLocation=
+      Enabled=true
+
       [GUI]
       ApplicationTheme=dark
       HidePasswords=true
+      MinimizeOnClose=true
+      MinimizeOnStartup=true
+      ShowTrayIcon=true
+      TrayIconAppearance=monochrome-light
+
+      [KeeShare]
+      Active="<?xml version=\"1.0\"?><KeeShare><Active/></KeeShare>\n"
+      QuietSuccess=true
 
       [PasswordGenerator]
       AdditionalChars=
