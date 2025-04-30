5bfc4de4f84a55603b7f2fc4eacb71c725acb449
Ekhorn <koen@kschellingerhout.nl>
2025-04-30 08:30:27 +0200

    conf(home): gnome extensions

--- modules/home-manager/dconf.nix
+++ modules/home-manager/dconf.nix
@@ -66,5 +66,35 @@
       command = "alacritty";
       name = "alacritty";
     };
+
+    "org/gnome/shell" = {
+      disabled-extensions = [
+        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+        "window-list@gnome-shell-extensions.gcampax.github.com"
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "native-window-placement@gnome-shell-extensions.gcampax.github.com"
+        "light-style@gnome-shell-extensions.gcampax.github.com"
+        "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+      ];
+      enabled-extensions = [
+        "system-monitor@gnome-shell-extensions.gcampax.github.com"
+        "status-icons@gnome-shell-extensions.gcampax.github.com"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
+      ];
+      welcome-dialog-last-shown-version = "47.2";
+    };
+
+    "org/gnome/shell/extensions/auto-move-windows" = {
+      application-list = [
+        "dev.zed.Zed.desktop:1"
+        "firefox.desktop:2"
+        "obsidian.desktop:3"
+      ];
+    };
   };
 }
