3ec6fce641769be2b2bbdb751b6049209128faa8
Ekhorn <koen@kschellingerhout.nl>
2025-06-29 15:45:50 +0200

    conf(git): add sum, continue, l, and ls alias

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -6,6 +6,15 @@
 
     aliases = {
       amend = "commit --amend --no-edit";
+      sum = "!git diff --oneline --stat $(git merge-base origin HEAD)..HEAD";
+      continue = "!f() { \
+        repo=$(git rev-parse --git-dir)
+        for file in rebase-merge rebase-apply MERGE_HEAD CHERRY_PICK_HEAD REVERT_HEAD AM_HEAD; do
+          [ -e \"$repo/$file\" ] && { cmd=\"\${file%[-_]*}\"; cmd=\"\${cmd,,}\"; git \${cmd//_/-} --continue; break; }
+        done;
+      }; f";
+      l = "log --stat --pretty=format:'%C(yellow)%h%Creset %Cgreen%cr%Creset %Cblue%an%Creset%C(auto)%d%Creset %s'";
+      ls = "!git log --stat --pretty=format:'%C(yellow)%h%Creset %Cgreen%cr%Creset %Cblue%an%Creset%C(auto)%d%Creset %s' $(git merge-base origin HEAD)..HEAD";
     };
 
     userEmail = "koen@kschellingerhout.nl";
