665985783ef6c17bb05bafc684e21e7571bafcbd
Ekhorn <koen@kschellingerhout.nl>
2025-08-20 17:18:04 +0200

    conf(latex): disable

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -14,7 +14,7 @@
   # hyprpaper = import ./hyprpaper.nix;
   java = import ./java.nix;
   keepassxc = import ./keepassxc.nix;
-  latex = import ./latex.nix;
+  # latex = import ./latex.nix;
   # rofi = import ./rofi.nix;
   tmux = import ./tmux.nix;
   vscode = import ./vscode.nix;
