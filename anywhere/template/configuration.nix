1ea10cafea1455f3b053d1e6242e01749d0838b0
Ekhorn <koen@kschellingerhout.nl>
2025-08-25 08:24:03 +0200

    conf(anywhere): add template

---
+++ anywhere/template/configuration.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  modulesPath,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./hardware-configuration.nix
+    inputs.disko.nixosModules.disko
+  ]
+  ++ (builtins.attrValues (import ../../modules/nixos/anywhere));
+
+  environment.systemPackages = map lib.lowPrio [ ];
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      # PasswordAuthentication = false;
+      # PermitRootLogin = "no";
+      # UsePAM = false;
+      # KbdInteractiveAuthentication = false;
+    };
+  };
+
+  user.enable = true;
+  user.username = "koen";
+  user.extraGroups = [
+    "wheel"
+    "docker"
+  ];
+
+  system.stateVersion = "25.05";
+}
