c14b45b7794f86e9dc1076605a6744c2766d5de7
Ekhorn <koen@kschellingerhout.nl>
2025-09-29 19:21:23 +0200

    conf: add cleanup cmd and refactor aliases

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -11,15 +11,27 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      build = "sudo nixos-rebuild test";
-      switch = "sudo nixos-rebuild switch";
+      build = "nixos-rebuild build";
       update = "sudo nixos-rebuild switch --recreate-lock-file";
+      switch = "sudo nixos-rebuild switch";
+      test = "sudo nixos-rebuild test";
     };
 
     initContent = ''
       dev() {
         # --impure to enable version selection
         NIXPKGS_COMMIT="$2" SHELL=$(which zsh) nix develop --impure $(readlink -f /etc/nixos/flake.nix)#$1 --command zsh
+      }
+      cleanup() {
+        # https://github.com/NixOS/nix/issues/8508#issuecomment-2808614321
+        user_profiles_garbage="/run/current-system/sw/bin/nix-collect-garbage"
+        if [ -z "$1" ]; then
+          sudo nix-collect-garbage
+          "$user_profiles_garbage" -d
+        else
+          sudo nix-collect-garbage --delete-older-than "$1"
+          "$user_profiles_garbage" --delete-older-than "$1"
+        fi
       }
     '';
 
