76f4c58c39f74f15d460b0829d95ad1371fa73ad
Ekhorn <koen@kschellingerhout.nl>
2025-10-10 23:39:52 +0200

    conf(minecraft): update server

--- anywhere/minecraft/minecraft.nix
+++ anywhere/minecraft/minecraft.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.minecraft-server = {
     enable = true;
     eula = true;
-    # openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
+    package = pkgs.minecraftServers.vanilla-1-21;
     declarative = true;
     whitelist = {
       # This is a mapping from Minecraft usernames to UUIDs. You can use https://mcuuid.net/ to get a Minecraft UUID for a username
@@ -13,11 +13,12 @@
     serverProperties = {
       server-port = 43000;
       difficulty = 3;
-      gamemode = 1;
+      gamemode = "survival";
       max-players = 5;
       motd = "NixOS Minecraft server!";
       white-list = true;
-      allow-cheats = true;
+      allow-cheats = false;
+      view-distance = 12;
     };
     jvmOpts = "-Xms2048M -Xmx4096M";
   };
