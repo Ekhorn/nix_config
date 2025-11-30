8f3395d2162c8dd8d44a96c5fe6cea3bc2c0b4d2
Ekhorn <koen@kschellingerhout.nl>
2025-11-30 15:20:11 +0100

    conf(git): editor nvim to helix

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -24,7 +24,7 @@
     signing.signByDefault = true;
 
     extraConfig = {
-      core.editor = "nvim";
+      core.editor = "hx";
       credential.helper = "store";
       format.commitMessageColumns = 72;
       init.defaultBranch = "main";
