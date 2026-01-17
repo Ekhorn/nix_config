815c83c29ff64027b9abff923c81425e4be7f0e1
Ekhorn <koen@kschellingerhout.nl>
2026-01-17 19:20:59 +0100

    fix(libfprint): comment out merged goodix driver patch

--- overlays/apply-patches.nix
+++ overlays/apply-patches.nix
@@ -1,5 +1,7 @@
 self: super: {
   libfprint = super.libfprint.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [ ../patches/goodix-60c2.patch ];
+    patches = [
+      # ../patches/goodix-60c2.patch merged in https://gitlab.freedesktop.org/libfprint/libfprint/-/commit/7d9638bc4375dbfee679aeb852d7a94b3ea22783
+    ];
   });
 }
