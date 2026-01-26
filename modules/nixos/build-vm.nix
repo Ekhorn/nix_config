522a1045d41d504b90fb1de304d47473dd26086a
Ekhorn <koen@kschellingerhout.nl>
2026-01-26 01:29:13 +0100

    conf(nixos): vm-build gtk,kvm,virtio display,passthrough

--- modules/nixos/build-vm.nix
+++ modules/nixos/build-vm.nix
@@ -5,11 +5,21 @@
   users.users.vmtest.initialPassword = "test";
   users.users.vmtest.group = "vmtest";
   users.groups.vmtest = { };
-
+  # vmVariant depends on: https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/virtualisation/qemu-vm.nix
+  # default qemu: options are set here https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/lib/qemu-common.nix
   virtualisation.vmVariant = {
     virtualisation = {
       memorySize = 2048;
       cores = 4;
     };
+    virtualisation.qemu.options = [
+      "-cpu host"
+      "-device virtio-rng-pci,rng=rng0"
+      "-display gtk,gl=on,grab-on-hover=on,show-cursor=on,zoom-to-fit=on"
+      "-enable-kvm"
+      "-machine pc,accel=kvm"
+      "-object rng-random,id=rng0,filename=/dev/urandom"
+      "-vga virtio"
+    ];
   };
 }
