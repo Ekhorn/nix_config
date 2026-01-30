bf417024462db41e34cf19d97936a21f1c8f53e8
Ekhorn <koen@kschellingerhout.nl>
2026-01-31 00:26:37 +0100

    conf(vm-build): overwrite default user and add guest

--- modules/nixos/build-vm.nix
+++ modules/nixos/build-vm.nix
@@ -1,15 +1,14 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
+let
+  user = "vmtest";
+in
 {
-  users.users.vmtest.isSystemUser = true;
-  users.users.vmtest.initialPassword = "test";
-  users.users.vmtest.group = "vmtest";
-  users.groups.vmtest = { };
   # vmVariant depends on: https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/virtualisation/qemu-vm.nix
   # default qemu: options are set here https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/lib/qemu-common.nix
   virtualisation.vmVariant = {
     virtualisation = {
-      memorySize = 2048;
+      memorySize = 2048; # 8192;
       cores = 4;
     };
     virtualisation.qemu.options = [
@@ -20,6 +19,26 @@
       "-machine pc,accel=kvm"
       "-object rng-random,id=rng0,filename=/dev/urandom"
       "-vga virtio"
+      # "-nographic"
     ];
+
+    # Guest user
+    users.users.guest = {
+      group = "guest";
+      initialPassword = "guest";
+      isSystemUser = true;
+      shell = pkgs.zsh;
+    };
+    users.groups.guest = { };
+
+    # Essential overwrites
+    user.username = lib.mkVMOverride user;
+    users.users.${user} = {
+      initialPassword = lib.mkVMOverride "test";
+    };
+    home-manager.users.vmtest.home.username = lib.mkVMOverride user;
+
+    # Non-essential overwrites
+    services.ollama.enable = lib.mkVMOverride false;
   };
 }
