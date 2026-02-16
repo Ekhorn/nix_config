a19339ab327af7ac14c571fde13662f25532eb4e
Ekhorn <koen@kschellingerhout.nl>
2026-02-16 09:09:03 +0100

    conf(home): opencode models

--- modules/home-manager/opencode.nix
+++ modules/home-manager/opencode.nix
@@ -6,8 +6,14 @@
     package = pkgs.unstable.opencode;
     settings = {
       autoshare = false;
-      autoupdate = true;
-      model = "llama2";
+      autoupdate = false;
+      model = "qwen2.5-coder:7b-32k";
+      permission = {
+        read = "allow";
+        edit = "ask";
+        bash = "ask";
+        webfetch = "ask";
+      };
       provider = {
         ollama = {
           npm = "@ai-sdk/openai-compatible";
@@ -16,9 +22,19 @@
             baseURL = "http://pc-koen:11434/v1";
           };
           models = {
-            "llama3.2:3b" = {
-              name = "Llama 3.2";
+            "qwen2.5-coder:3b-32k" = {
+              tools = true;
             };
+            "qwen2.5-coder:7b-32k" = {
+              tools = true;
+            };
+            "qwen2.5-coder:14b-32k" = {
+              tools = true;
+            };
+            "gpt-oss:20b" = { };
+            "llama3.2:3b" = { };
+            "qwen3-vl:4b" = { };
+            "qwen3-vl:8b" = { };
           };
         };
       };
