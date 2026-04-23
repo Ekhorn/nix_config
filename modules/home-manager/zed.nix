ee200efc430cd27de91cf3e67bc17a9ae2e50da8
Ekhorn <koen@kschellingerhout.nl>
2026-04-23 09:06:21 +0200

    conf(zed): add agent models

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -96,6 +96,24 @@
           };
         };
       };
+
+      agent = {
+        default_model = {
+          provider = "google";
+          model = "gemini-3-flash-preview";
+        };
+        favorite_models = [
+          {
+            provider = "google";
+            model = "gemini-3-flash-preview";
+          }
+          {
+            provider = "google";
+            model = "gemini-3.1-pro-preview";
+            enable_thinking = true;
+          }
+        ];
+      };
     };
   };
 }
